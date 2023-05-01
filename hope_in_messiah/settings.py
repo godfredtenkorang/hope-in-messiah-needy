4c1f698d75454d3c8a9753dbfa9869e1f11b8a14
godeytech <godfredtenkorang9@gmail.com>
2023-05-01 21:31:36 +0000

    Debug to false

--- hope_in_messiah/settings.py
+++ hope_in_messiah/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1bgm*_7+831xs2deemlufo52sx^k03+wrql&)k(a5h616qni6c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.himfoundation.net', '164.92.180.205', 'localhost', '127.0.0.1']
 
