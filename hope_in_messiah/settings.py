d730fad3410c862730814a792cf82a4046e59d99
godeytech <godfredtenkorang9@gmail.com>
2023-05-01 22:20:53 +0000

    removed postgres DB

--- hope_in_messiah/settings.py
+++ hope_in_messiah/settings.py
@@ -79,25 +79,13 @@ WSGI_APPLICATION = 'hope_in_messiah.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'hope_in_messiah',
-            'USER': 'hope',
-            'PASSWORD': 'Gony@100',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
