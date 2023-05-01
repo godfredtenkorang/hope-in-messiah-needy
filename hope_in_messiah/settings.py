d3a62bd6a9bb11f92f6cdf530fffac38c86a2b1e
godeytech <godfredtenkorang9@gmail.com>
2023-05-01 21:29:07 +0000

    Postgres DB changes

--- hope_in_messiah/settings.py
+++ hope_in_messiah/settings.py
@@ -79,13 +79,25 @@ WSGI_APPLICATION = 'hope_in_messiah.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'hope_in_messiah',
+            'USER': 'hope',
+            'PASSWORD': 'Gony@100',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
