04d8ebb6736684a7ce68e03aec2e95a111ce16af
godeytech <godfredtenkorang9@gmail.com>
2023-05-01 19:51:39 +0000

    added ip address

--- hope_in_messiah/settings.py
+++ hope_in_messiah/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1bgm*_7+831xs2deemlufo52sx^k03+wrql&)k(a5h616qni6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.himfoundation.net', '161.35.27.66', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.himfoundation.net', '164.92.180.205', 'localhost', '127.0.0.1']
 
 
 # Application definition
