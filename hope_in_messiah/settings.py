a900105f9e667372fd8d07d5a5c2757049387a68
godeytech <godfredtenkorang9@gmail.com>
2023-05-02 02:57:18 +0000

    Comit

--- hope_in_messiah/settings.py
+++ hope_in_messiah/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'hope_in_messiah.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
     
-# else:
-DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'hope_in_messiahdb',
-    'USER': 'hope',
-    'PASSWORD': 'gony@100',
-    'HOST': 'localhost',
-    'PORT': '',
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hope_in_messiahdb',
+        'USER': 'hope',
+        'PASSWORD': 'gony@100',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
