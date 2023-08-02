765b79043965de38ef6a1e385e6bb9132894542b
godeytech <godfredtenkorang9@gmail.com>
2023-08-02 15:17:15 +0000

    added requirement.txt

--- hope_in_messiah/settings.py
+++ hope_in_messiah/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ['.himfoundation.net','147.182.172.131', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.himfoundation.net','167.172.139.82', 'localhost', '127.0.0.1']
 
 
 # Application definition
