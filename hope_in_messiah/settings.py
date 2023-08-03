95e4e5b1df9db31a13584a3495b40870f2dea73e
godeytech <godfredtenkorang9@gmail.com>
2023-08-03 08:55:09 +0000

    added ig url

--- hope_in_messiah/settings.py
+++ hope_in_messiah/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ['.himfoundation.net','104.236.69.64', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['.himfoundation.net','68.183.134.2', 'localhost', '127.0.0.1']
 
 
 # Application definition
