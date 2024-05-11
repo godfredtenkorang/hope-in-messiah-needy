e5e02b90df6921642afc8f8e1f42b1c46b8f3d3d
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2024-05-11 17:40:34 +0000

    changes

--- hope_in_messiah/settings.py
+++ hope_in_messiah/settings.py
@@ -82,24 +82,24 @@ WSGI_APPLICATION = 'hope_in_messiah.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # if DEBUG:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-    
-# else
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'godey',
-#         'USER': 'godey',
-#         'PASSWORD': 'Gony@100',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+    
+# else
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'godey',
+        'USER': 'godey',
+        'PASSWORD': 'Gony@100',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
